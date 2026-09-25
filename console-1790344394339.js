8b70de341a51f5f4f699d08833a135cf0cdfe18a
baggioman <baggiomans@gmail.com>
2026-09-25 20:56:24 +0700

    Add files via upload

---
+++ console-1790344394339.js
@@ -0,0 +1,25 @@
+/assets/index-BfR246md.js:2870 Error activating license
+G @ /assets/index-BfR246md.js:2870
+/assets/index-BfR246md.js:2870 ReferenceError: Cannot access 'z0' before initialization
+    at callback (/assets/index-BfR246md.js:2870)
+    at G (/assets/index-BfR246md.js:2870)
+    at p0 (/assets/index-BfR246md.js:2870)
+    at /assets/index-BfR246md.js:2870
+    at e.exports.execScript (uxp://uxp-internal/domjs_scripts.js:2)
+    at l (uxp://uxp-internal/domjs_scripts.js:2)
+    at uxp://uxp-internal/domjs_scripts.js:2
+    at s._executeCallbacks (uxp://uxp-internal/domjs_scripts.js:2)
+    at s._executeIfReady (uxp://uxp-internal/domjs_scripts.js:2)
+    at Array.<anonymous> (uxp://uxp-internal/domjs_scripts.js:2)
+G @ /assets/index-BfR246md.js:2870
+/assets/index-BfR246md.js:2870 Panel Shown
+uxp://uxp-internal/domjs_scripts.js:2 Uncaught TypeError: window.focus is not a function
+    at X.<anonymous> (/assets/index-BfR246md.js:2870)
+    at uxp://uxp-internal/domjs_scripts.js:2
+    at P (uxp://uxp-internal/domjs_scripts.js:2)
+    at uxp://uxp-internal/domjs_scripts.js:2
+    at D (uxp://uxp-internal/domjs_scripts.js:2)
+    at L (uxp://uxp-internal/domjs_scripts.js:2)
+    at dispatchNativeEvent (uxp://uxp-internal/domjs_scripts.js:2)
+    at e.exports._retargetAndDispatchNativeEvent (uxp://uxp-internal/domjs_scripts.js:2)
+    at e.exports.onEvent (uxp://uxp-internal/domjs_scripts.js:2)
